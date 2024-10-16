47da8f7789eed3739056b3573a3d99d5dcba474b
wezbest <110793713+wezbest@users.noreply.github.com>
2024-10-16 05:11:59 +0000

    🫦

--- RUN/r1.py
+++ RUN/r1.py
@@ -1,11 +1,33 @@
 # /// script
 # requires-python = ">=3.12"
-# dependencies = []
+# dependencies = [
+#     "rich",
+# ]
 # ///
+
+from rich.console import Console
+from rich.panel import Panel
+
+console = Console()
 
 
 def main() -> None:
     print("Hello from r1.py!")
+    panel()
+
+
+def panel() -> None:
+    panel = Panel(
+        """
+Enjoy StinlySmellySWeaty women
+""",
+        title="Mistress",
+        subtitle="ToiletSlave",
+        style="Italic",
+        border_style="magenta",
+    )
+    # Print the Panel
+    console.print(panel)
 
 
 if __name__ == "__main__":
