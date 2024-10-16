247c62c4442e87de1d4240e778ce644d1a24122b
wezbest <110793713+wezbest@users.noreply.github.com>
2024-10-16 05:30:25 +0000

    🫦

--- RUN/r2/hello.py
+++ RUN/r2/hello.py
@@ -1,5 +1,33 @@
-def main():
-    print("Hello from r2!")
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "rich",
+# ]
+# ///
+
+from rich.console import Console
+from rich.panel import Panel
+
+console = Console()
+
+
+def main() -> None:
+    print("Hello from r1.py!")
+    panel()
+
+
+def panel() -> None:
+    panel = Panel(
+        """
+Enjoy StinlySmellySWeaty women
+""",
+        title="Mistress",
+        subtitle="ToiletSlave",
+        style="Italic",
+        border_style="magenta",
+    )
+    # Print the Panel
+    console.print(panel)
 
 
 if __name__ == "__main__":
